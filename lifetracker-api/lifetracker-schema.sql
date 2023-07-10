a4bd6598674816ebe9222707e0467aa52cd244e0
emandats <125905415+emandats@users.noreply.github.com>
2023-07-09 23:38:38 -0400

    Add files via upload

---
+++ lifetracker-api/lifetracker-schema.sql
@@ -0,0 +1,8 @@
+CREATE TABLE users (
+  id         SERIAL PRIMARY KEY,
+  username   TEXT NOT NULL,
+  password   TEXT NOT NULL,
+  first_name TEXT NOT NULL,
+  last_name  TEXT NOT NULL,
+  email      TEXT NOT NULL UNIQUE CHECK (position('@' IN email) > 1)
+);
